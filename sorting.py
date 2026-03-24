dbd512745909725c451616900a5450c77ebd7f84
Sai Swaroop Ghadai <ghadai.saiswaroop@proton.me>
2026-03-24 22:30:08 +0530

    Add insertion sort method

--- sorting.py
+++ sorting.py
@@ -1,3 +1,5 @@
+numList = [64, 34, 25, 12, 22, 11, 90]  #List of unsorted numbers
+
 #Bubble Sort in Python
 
 def bubble_sort(list1):
@@ -8,10 +10,23 @@ def bubble_sort(list1):
                 list1[j], list1[j+1] = list1[j+1], list1[j]
 
 
-numList = [64, 34, 25, 12, 22, 11, 90]  #List of unsorted numbers
 bubble_sort(numList)                    #Calling the function to sort the list
 print("Sorted list is:", numList)       #Printing the sorted list
 
 #===========================================================================================
+
 #Insertion Sort in Python
 
+def insertion_sort(list1):
+    n = len(list1)
+    for i in range(1, n):
+        check = list1[i]
+        j = i-1
+        while j>=0 and check<list1[j]:
+            list1[j+1]=list1[j]
+            j=j-1
+        else:
+            list1[j+1]=check
+
+insertion_sort(numList)                    #Calling the function to sort the list
+print("Sorted list is:", numList)          #Printing the sorted list
